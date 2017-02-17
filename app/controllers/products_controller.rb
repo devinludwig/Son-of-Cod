009c6616e2fb12d8d065544f1da2824a9222ce7b
Devin Ludwig <devinludwig@gmail.com>
2017-02-17 11:27:17 -0800

    add views for CRUD functionality of products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_picture, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
